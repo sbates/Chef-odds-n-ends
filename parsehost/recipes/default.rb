71cbdd13e1fc9888e303167395903c0c39e77ece
Sascha Bates <sascha@brattyredhead.com>
2011-06-26 10:30:59 -0500

    adding a parsehost cookbook with a library/recipe

---
+++ parsehost/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: parsehost
+# Recipe:: default
+#
+# Copyright 2011, Best Buy
+#
+# All rights reserved - Do Not Redistribute
+#
---
+++ parsehost/recipes/parsehost.rb
@@ -0,0 +1,39 @@
+# Cookbook Name:: parsehost
+# Recipe:: parsehost
+#
+# Copyright 2011
+#
+# All rights reserved - Do Not Redistribute
+#
+
+def parse_host_function(hostname)
+
+    Chef::Log.info("HostParse: parse_host: parsing host #{hostname}")
+
+    parser = HostParser.new
+    parser.match_host(hostname)
+    parser.set_extra_bits
+    parser.assign_hostdata
+
+    Chef::Log.debug("logging the hostdata hash pairs:")
+ return parser.hostdata
+end
+
+# an example of how you might use this function or use HostParser directly
+# if you had, say, a new a set of new servers not yet created that you might want to create seed nodes for
+someservers = ["dlplolwmq01", "dxplolweb03", "pvpafpapp01"]
+parsedata = ""
+someservers.each do |srv| 
+  parsedata << "#{srv}:" << parse_host_function(srv).inspect << "\n"
+end
+
+file "/tmp/hostdata.txt" do
+  content parsedata
+end
+
+# Output:
+# [root@dvdchfapp05 ~]# cat /tmp/hostdata.txt
+# pvplolwmq01:{:application=>"Cheez", :zone=>"internal", :platform=>"linux", :environment=>"prod", :env=>"p", :server_type=>"mq", :location=>"pismo", :loc=>"d"}
+# pxplolweb03:{:application=>"Cheez", :zone=>"internal", :platform=>"Solaris", :environment=>"prod", :env=>"p", :server_type=>"web", :location=>"pismo", :loc=>"d"}
+# pvppkyapp01:{:application=>Pooky, :zone=>"external", :platform=>"linux", :environment=>"prod", :env=>"p", :server_type=>"application", :location=>"pismo", :loc=>"d"}
+
